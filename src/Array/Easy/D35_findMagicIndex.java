77b75b8f48b84862ed18b274a54fe242a2a1c86a
2gb0101 <b_2gb0101@163.com>
2021-02-22 22:30:02 +0800

    2021-02-22

--- src/Array/Easy/D35_findMagicIndex.java
+++ src/Array/Easy/D35_findMagicIndex.java
@@ -1,4 +1,37 @@
 package Array.Easy;
 
+//在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i
+//Offer: 一个有序整数数组
+//Target: 编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1
+//        若有多个魔术索引，返回索引值最小的一个
 public class D35_findMagicIndex {
+//    解题思路：使用二分+递归
+//    此题目乍一看一个for循环就解决啊
+//    再仔细瞧一眼，有序数组，到底是升序还是降序呢？
+//    数组有序上二分，立马设置left = 0， right = nums.length - 1
+//    如果mid刚好等于nums[mid]，那就往左边搜索（求最小的魔法索引）
+//    如果mid不等于nums[mid]的话
+//        那这个魔法索引可能是在左边，也可能是在右边
+//        所以你不能光搜索某一边的数字（许多解答通过测试，却有漏洞大概就是这个原因）
+//    既然要重复搜索各个部分的话，上递归吧(因为可能有多个魔术索引，所以要搜索各个部分)
+    int ans = -1;
+    public int findMagicIndex(int[] nums) {
+        search(nums, 0, nums.length - 1);
+        return ans;
+    }
+
+    private void search(int[] nums, int left, int right){
+        if(left > right) return;
+
+        int mid = (left + right) / 2;
+
+        if(nums[mid] == mid){
+            ans = mid;
+            search(nums, left, mid - 1);
+        }
+        else {
+            search(nums, left, mid - 1);
+            if(ans == -1) search(nums, mid + 1, right);
+        }
+    }
 }
--- src/Array/Easy/D35_masterMind.java
+++ src/Array/Easy/D35_masterMind.java
@@ -1,4 +1,70 @@
 package Array.Easy;
 
+//计算机有4个槽，每个槽放一个球，颜色可能是红色（R）、黄色（Y）、绿色（G）或蓝色（B）
+//例如，计算机可能有RGGB 4种（槽1为红色，槽2、3为绿色，槽4为蓝色）
+//作为用户，你试图猜出颜色组合
+//打个比方，你可能会猜YRGB。要是猜对某个槽的颜色，则算一次“猜中”；
+//要是只猜对颜色但槽位猜错了，则算一次“伪猜中”。注意，“猜中”不能算入“伪猜中”
+//
+//Offer：一种颜色组合solution和一个猜测guess
+//Target：编写一个方法，返回猜中和伪猜中的次数answer，其中answer[0]为猜中的次数，answer[1]为伪猜中的次数
+
+import java.util.HashMap;
+
+//示例：
+//输入： solution="RGBY",guess="GGRR"
+//输出： [1,1]
+//解释： 猜中1次，伪猜中1次。
 public class D35_masterMind {
+//    解题思路1
+//    因为solution和guess的长度都只有4，所以嵌套for循环暴力解的话，也很快
+
+//    解题思路2
+//    设置一个长度为26的数组map（目的是将RYGB对应到数组的index中）
+//    for循环遍历solution和guess
+//    如果solution和guess对应元素相等，则直接real++
+//    若不相等，判断map中sol元素是否小于0（代表之前存过guess的元素），存在则fake++，然后更新map[sol - 'A']++;
+//    对map中的gue元素做同等判断
+//    返回答案ans
+    public int[] masterMind1(String solution, String guess) {
+        int fake = 0, real = 0;
+        int[] map = new int[26];
+        for(int i = 0; i < 4; i++){
+            char sol = solution.charAt(i), gue = guess.charAt(i);
+            if(sol == gue) real++;
+            else{
+                if(map[sol - 'A'] < 0) fake++;
+                map[sol - 'A']++;
+
+                if(map[gue - 'A'] > 0) fake++;
+                map[gue - 'A']--;
+            }
+        }
+        int[] ans = {real, fake};
+        return ans;
+    }
+
+//    解题思路3
+//    使用了HashMap，将solution的元素保存到map中（包含元素数量）
+//    然后判断map中是否有guess的元素，有则fake++，注意要更新元素数量
+//    在来个for循环判断一致的数据real，最后fake - real等于伪猜对
+    public int[] masterMind2(String solution, String guess) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(char c : solution.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int fake = 0, real = 0;
+        for(char c : guess.toCharArray()){
+            if(map.containsKey(c) && map.get(c) > 0){
+                fake++;
+                map.put(c, map.get(c) - 1);
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            if(solution.charAt(i) == guess.charAt(i))
+                real++;
+        }
+        int[] ans = {real, fake - real};
+        return ans;
+    }
 }
